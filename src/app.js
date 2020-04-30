124351c56d03048176ec8c0f02b827fe38f84781
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2020-05-01 00:03:33 +0700

    Initialize html structure

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import "regenerator-runtime";
+
+import "./style/style.css";
+import img from "./image/newspaper.jpg";
+import icon1 from "./image/chip.svg";
+import icon2 from "./image/graphic.svg";
+import icon3 from "./image/heart.svg";
+import icon4 from "./image/news.svg";
+import icon5 from "./image/science.svg";
+import icon6 from "./image/search.svg";
+import icon7 from "./image/soccer.svg";
+import icon8 from "./image/videocamera.svg";
